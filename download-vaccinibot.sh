3a8198e106a1308fff1662bce27a4dadc081dd06
Aldo97 <67073034+Aldo97@users.noreply.github.com>
2021-09-21 13:21:04 +0200

    Update download-vaccinibot.sh

--- download-vaccinibot.sh
+++ download-vaccinibot.sh
@@ -11,6 +11,7 @@ fi
 curl https://raw.githubusercontent.com/Aldo97/VacciniBot/main/vaccinibot.py --silent --output vaccinibot.py
 curl https://raw.githubusercontent.com/Aldo97/VacciniBot/main/istat21.csv --silent --output istat21.csv
 curl https://raw.githubusercontent.com/Aldo97/VacciniBot/main/start-vaccinibot.sh --silent --output start-vaccinibot.sh
+curl https://raw.githubusercontent.com/Aldo97/VacciniBot/main/update-vaccinibot.sh --silent --output update-vaccinibot.sh
 
 sed -i '27s/.*/TOKEN = "'$Token'"/' vaccinibot.py
 sed -i '29s/.*/cid = '$cid'/' vaccinibot.py
